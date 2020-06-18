e08df2b53ae509da0e01d9d20f5d562253090838
Oyakzon <marcos_oyarzo97@hotmail.com>
2020-06-18 19:01:06 -0400

    capitulo 15 listado y paginación completado

--- resources/views/ventas/cliente/index.blade.php
+++ resources/views/ventas/cliente/index.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
 	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
 		<h3>Listado de Clientes <a href="cliente/create"><button class="btn btn-success">Nuevo</button></a></h3>
-		@include('ventas.clientes.search')
+		@include('ventas.cliente.search')
 	</div>
 </div>
 
