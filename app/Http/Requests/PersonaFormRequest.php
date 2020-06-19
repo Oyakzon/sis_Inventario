6bfe5415cd9c6d61ac5787e1f38e5d225aa4419b
Sebastian2134 <sebastian.ipchile@gmail.com>
2020-06-18 22:35:05 -0400

    Creacion de la opcion Articulo completa

--- app/Http/Requests/PersonaFormRequest.php
+++ app/Http/Requests/PersonaFormRequest.php
@@ -2,9 +2,9 @@
 
 namespace sis_Inventario\Http\Requests;
 
-use sis_Inventario\Http\Requests\Requests;
+use sis_Inventario\Http\Requests\Request;
 
-class ArticuloFormRequest extends Request
+class PersonaFormRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,13 +24,12 @@ class ArticuloFormRequest extends Request
     public function rules()
     {
         return [
-            'idcategoria'=>'required',
-            'codigo'=>'required|max:50',
             'nombre'=>'required|max:100',
-            'stock'=>'required|numeric',
-            'descripcion'=>'max:512',
-            'imagen'=>'mimes:jpeg,bmp,png',
-
+            'tipo_documento'=>'required|max:20',
+            'num_documento'=>'required|max:15',
+            'direccion'=>'required|max:70',
+            'telefono'=>'required|max:15',
+            'email'=>'required|max:50'
         ];
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,9 @@ Route::get('/', function () {
 });
 
 Route::resource('almacen/categoria','CategoriaController');
+
+Route::resource('almacen/articulo','ArticuloController');
+
+Route::resource('ventas/cliente','ClienteController');
+
+
