481044003070246eaff01c3f8c900e78cd5863a0
Sebastian2134 <sebastian.ipchile@gmail.com>
2020-06-23 12:37:24 -0400

    rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,20 +16,17 @@ Route::get('/', function () {
 });
 
 Route::resource('almacen/categoria','CategoriaController');
-
 Route::resource('almacen/articulo','ArticuloController');
-
 Route::resource('ventas/cliente','ClienteController');
-
 Route::resource('compras/proveedor','ProveedorController');
-
 Route::resource('seguridad/usuario','UsuarioController');
+Route::resource('compras/ingreso','IngresoController');
+Route::resource('ventas/venta','VentaController');
 
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+Route::get('/{slug?}', 'HomeController@index');
 
 
-Route::resource('compras/ingreso','IngresoController');
 
-Route::resource('ventas/venta','VentaController');
